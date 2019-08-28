e544fc6518c2257859bfa0d0aac35a4e5ba40ad2
AmirhoseinDelavar <am.delavar@gmail.com>
2019-08-28 11:15:39 +0430

    LocalStorage added

--- jsfile.js
+++ jsfile.js
@@ -1,26 +1,25 @@
-// making dynamic contents on html
-const todos = [{
-    text: 'eat',
-    complete: false
-}, {
-    text: 'walk',
-    complete: true
-}, {
-    text: 'run',
-    complete: false
-}]
+// Loading the todos from disk
+let todos = []
+const JSONData = localStorage.getItem('todos')
+todos = JSON.parse(JSONData)
+if (todos === null)
+    todos = []
+//
 
 // Creating search field :
-const searchFilter ={
-    searchtext : ''
+const searchFilter = {
+    searchtext: '',
+    hideCompleted: false
 }
-document.querySelector('#search-in').addEventListener('input',function (e) {
+document.querySelector('#search-in').addEventListener('input', function (e) {
     searchFilter.searchtext = e.target.value
-    SearchEngine(todos, searchFilter)
+    RenderNotes(todos, searchFilter)
 })
-const SearchEngine = function (todos, searchFilter) {
+const RenderNotes = function (todos, searchFilter) {
     const filteredTodos = todos.filter(function (todo) {
-        return todo.text.toLowerCase().includes(searchFilter.searchtext.toLowerCase())
+        const searchtextIsValid = todo.text.toLowerCase().includes(searchFilter.searchtext.toLowerCase())
+        const hidden = searchFilter.hideCompleted && todo.complete
+        return searchtextIsValid && !hidden
     })
     document.querySelector('#search-out').innerHTML = ''
     filteredTodos.forEach(function (todo) {
@@ -28,35 +27,38 @@ const SearchEngine = function (todos, searchFilter) {
         todotag.textContent = todo.text
         document.querySelector('#search-out').append(todotag)
     })
+
+    // todos left
+    document.querySelector('#left-todo').innerHTML = ''
+    document.querySelector('#left-todo').append(`You Have ${todos.filter(function (todo) {
+        return !todo.complete
+    }).length} Left`)
+    //
+
 }
 //
 
+RenderNotes(todos, searchFilter)
+
 // Creating Newtodo btn
-document.querySelector('#new-todo-form').addEventListener('submit',function (e) {
+document.querySelector('#new-todo-form').addEventListener('submit', function (e) {
     e.preventDefault();
     const newtodo = {
         text: e.target.elements.todotext.value,
         complete: false
     }
     todos.push(newtodo)
+    localStorage.removeItem('todos')
+    localStorage.setItem('todos',JSON.stringify(todos))
     e.target.elements.todotext.value = ''
-    SearchEngine(todos,searchFilter)
+    RenderNotes(todos, searchFilter)
 })
 //
 
-// todos left
-document.querySelector('#left-todo').append(`You Have ${todos.filter(function (todo) {
-    return !todo.complete
-}).length} Left`)
-//
 
-// Creating the show-checkbox
-document.querySelector('#show-list-in').addEventListener('change', function (e) {
-    console.log(e.target.checked)
-    if (e.target.checked)
-        SearchEngine(todos,searchFilter)
-    else
-        document.querySelector('#search-out').innerHTML = ''
-
+// Creating the hide-completed-checkbox
+document.querySelector('#hide-completed-ch').addEventListener('change', function (e) {
+    searchFilter.hideCompleted = e.target.checked
+    RenderNotes(todos, searchFilter)
 })
 //
